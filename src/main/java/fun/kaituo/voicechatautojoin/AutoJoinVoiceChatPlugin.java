9586c14f155f21c951ab3f1da37eb4e5990f16aa
YFshadaow <renzd2001@gmail.com>
2025-01-12 10:49:55 +0800

    Restructure code

--- src/main/java/fun/kaituo/voicechatautojoin/AutoJoinVoiceChatPlugin.java
+++ src/main/java/fun/kaituo/voicechatautojoin/AutoJoinVoiceChatPlugin.java
@@ -4,15 +4,24 @@ import de.maxhenkel.voicechat.api.Group;
 import de.maxhenkel.voicechat.api.VoicechatPlugin;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import de.maxhenkel.voicechat.api.events.PlayerConnectedEvent;
-import de.maxhenkel.voicechat.api.events.ServerEvent;
+import de.maxhenkel.voicechat.api.events.VoicechatServerStartedEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class AutoJoinVoiceChatPlugin implements VoicechatPlugin {
     private final VoiceChatAutoJoin plugin;
 
-    private static final UUID uuid = UUID.randomUUID();
+    public static final String PLUGIN_ID = "voicechatautojoin";
+
+    public static final String DEFAULT_GROUP_NAME = "Default";
+    public static final UUID DEFAULT_GROUP_UUID = UUID.fromString("e723bdf9-f7e3-4099-a1c9-03a1d77402e4");
+    public static final String DEFAULT_JOIN_MESSAGE = "§f已将你加入默认语音聊天频道！";
+
+    public static final int CLEAR_GROUPS_DELAY = 20; // In server ticks
 
     public AutoJoinVoiceChatPlugin(VoiceChatAutoJoin plugin) {
         this.plugin = plugin;
@@ -20,44 +29,43 @@ public class AutoJoinVoiceChatPlugin implements VoicechatPlugin {
 
     @Override
     public String getPluginId() {
-        return VoiceChatAutoJoin.PLUGIN_ID;
+        return PLUGIN_ID;
     }
 
-    private boolean doesGroupExist(ServerEvent e) {
-        for (Group g : e.getVoicechat().getGroups()) {
-            if (g.getId().equals(uuid)) {
-                return true;
-            }
-        }
-        return false;
+    public void onServerStarted(VoicechatServerStartedEvent e) {
+        plugin.setApi(e.getVoicechat());
+        Bukkit.getScheduler().runTaskLater(plugin, this::initializeGroups, CLEAR_GROUPS_DELAY);
     }
 
-    private void createGroupIfNotExists(ServerEvent e) {
-        if (!doesGroupExist(e)) {
-            plugin.getLogger().info("Does not exist, creating group");
-            e.getVoicechat().groupBuilder()
-                    .setId(uuid)
-                    .setName(plugin.getConfig().getString("group-name"))
-                    .setPersistent(true)
-                    .setType(Group.Type.OPEN)
-                    .build();
+    public void initializeGroups() {
+        // Remove all groups
+        List<UUID> groupIds = new ArrayList<>();
+        for (Group group : plugin.getApi().getGroups()) {
+            groupIds.add(group.getId());
         }
+        for (UUID groupId : groupIds) {
+            plugin.getApi().removeGroup(groupId);
+        }
+        // Create default and spectator groups
+        plugin.getApi().groupBuilder()
+                .setId(DEFAULT_GROUP_UUID)
+                .setName(DEFAULT_GROUP_NAME)
+                .setPersistent(true)
+                .setType(Group.Type.ISOLATED)
+                .build();
     }
 
     public void onPlayerConnected(PlayerConnectedEvent e) {
-        createGroupIfNotExists(e);
-        e.getConnection().setGroup(e.getVoicechat().getGroup(uuid));
+        e.getConnection().setGroup(e.getVoicechat().getGroup(DEFAULT_GROUP_UUID));
         Player p = (Player) e.getConnection().getPlayer().getPlayer();
-        String msg = "§f已将你自动加入全服语音聊天频道！";
-        p.sendMessage(msg);
+        p.sendMessage(DEFAULT_JOIN_MESSAGE);
     }
-
 
 
     @Override
     public void registerEvents(EventRegistration registration) {
         registration.registerEvent(PlayerConnectedEvent.class, this::onPlayerConnected);
+        registration.registerEvent(VoicechatServerStartedEvent.class, this::onServerStarted);
     }
-
 
 }
--- src/main/java/fun/kaituo/voicechatautojoin/VoiceChatAutoJoin.java
+++ src/main/java/fun/kaituo/voicechatautojoin/VoiceChatAutoJoin.java
@@ -1,15 +1,23 @@
 package fun.kaituo.voicechatautojoin;
 
 import de.maxhenkel.voicechat.api.BukkitVoicechatService;
+import de.maxhenkel.voicechat.api.VoicechatServerApi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class VoiceChatAutoJoin extends JavaPlugin {
 
-    public static final String PLUGIN_ID = "voicechatautojoin";
+    private VoicechatServerApi api;
+
+    public VoicechatServerApi getApi() {
+        return api;
+    }
+
+    public void setApi(VoicechatServerApi api) {
+        this.api = api;
+    }
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
         BukkitVoicechatService service = getServer().getServicesManager().load(BukkitVoicechatService.class);
         if (service != null) {
             AutoJoinVoiceChatPlugin voiceChatPlugin = new AutoJoinVoiceChatPlugin(this);
